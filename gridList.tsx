d1d5a33d239fbca68b35799a0999755473641afb
Ibrahim-Sefa-Saglam <saglamibrahimsefa@gmail.com>
2024-12-28 13:23:55 +0300

    1

---
+++ gridList.tsx
@@ -0,0 +1,19 @@
+interface stateProps{
+    grid: Array<Array<string>>;
+    heuristicValue: Number;
+  }
+
+let initialState: stateProps; 
+
+const gridList = () =>{
+
+
+
+}
+
+const bringGrids = (currentState:stateProps) : Array<Array<string>>[] | void  =>{
+
+
+    
+
+}
